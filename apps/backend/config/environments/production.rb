0f6c10d0b67d6be09491b3386c4b8f30ee2a6c12
Xavier Foucron <73240020+xfoucron@users.noreply.github.com>
2026-02-27 00:19:54 +0100

    feat(ci): add build to backend (#8)
    * feat(ci): add build to backend
    
    * fix(ci): add permissions to build
    
    * fix(ci): context
    
    * fix(ci): image name
    
    * feat: add helm chart to deploy apps
    
    * fix(backend): disable cache in production
    
    * feat: add env support

--- apps/backend/config/environments/production.rb
+++ apps/backend/config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Replace the default in-process memory cache store with a durable alternative.
-  config.cache_store = :solid_cache_store
+  config.cache_store = :null_store
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
